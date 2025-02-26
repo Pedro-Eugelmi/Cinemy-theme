f316954743cb40baf6816faf8812c4cd4495859f
pedro eugelmi <pedrogabrielmendy@gmail.com>
2025-02-25 22:01:24 -0300

    New wordpress theme

---
+++ config/setup.php
@@ -0,0 +1,24 @@
+<?php
+
+// Prevent direct access
+if (!defined('ABSPATH')) exit;
+
+// add styles and scripts
+
+function theme_enqueue_scripts() {
+    // Styles
+    wp_enqueue_style('bootstrap_css', get_template_directory_uri(). '/styles/minified/bootstrap.min.css', array(), filemtime(get_template_directory() . '/styles/minified/bootstrap.min.css'));
+    wp_enqueue_style('reset', get_template_directory_uri(). '/styles/minified/reset.css', array(), filemtime(get_template_directory() . '/styles/minified/reset.css'));
+    wp_enqueue_style('default', get_template_directory_uri(). '/styles/minified/default.min.css', array(), filemtime(get_template_directory() . '/styles/minified/default.min.css'));
+    wp_enqueue_style('style', get_template_directory_uri(). '/styles/minified/style.min.css', array(), filemtime(get_template_directory() . '/styles/minified/style.min.css'));
+
+    // Scripts
+    wp_enqueue_script('bootstrap_js', get_template_directory_uri() . '/scripts/bootstrap.bundle.min.js', array(), filemtime(get_template_directory() . '/scripts/bootstrap.bundle.min.js'), true);
+    wp_enqueue_script('script', get_template_directory_uri() . '/scripts/script.js', array(), filemtime(get_template_directory() . '/scripts/script.js'), true);
+}
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
+
+// Remove tool bar
+
+add_filter('show_admin_bar', '__return_false');
---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php   
+
+//  Enqueue the scripts and style / setup functions  
+
+require_once get_template_directory() . '/config/setup.php';
+
+// Add theme supports 
+
+require_once get_template_directory() . '/config/theme-support.php';
+
+// Add menus 
+
+require_once get_template_directory() . '/config/menus.php';
+
+// Woocommerce functions 
+
+if ( class_exists( 'WooCommerce' ) ) {
+    require_once get_template_directory() . '/config/shop.php';
+}
